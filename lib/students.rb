fc6abef25cefa68268ec7572de13ab3d22770b59
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-08 22:43:24 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,7 +9,7 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM students WHERE grade = 9;"
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
@@ -17,4 +17,5 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9;"
 end
